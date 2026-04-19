121848abc39b3dc187c838eedb8ec2f3dfe7af7a
anshsingh1032 <thansh1032@gmail.com>
2026-04-19 23:13:13 +0530

    Configure CORS to allow frontend access

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,10 +6,21 @@ const app = express()
 app.use(express.json({
     limit:"16kb"
 }))
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cast-your-vote-jet.vercel.app"
+];
+
 app.use(cors({
-    origin: 'http://localhost:5174', 
-    credentials: true
-}))
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.urlencoded({
     extended:true,limit:"16kb"
 }))
